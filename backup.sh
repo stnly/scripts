a5320f751b4855f6f77f0f48aea74c0f45b21535
Stanley Tan <me@stnly.com>
2011-11-19 20:02:43 +0800

    Edit

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Create mysql backup.$(date +%Y-%m-%d) appends the current date to the file name.
-mysqldump --opt -Q -u dbuser --password=dbpassword dbname > ~/sql-$(date +%Y-%m-%d).sql
+mysqldump --opt -Q -u dbusername --password=dbpassword dbname > ~/sql-$(date +%Y-%m-%d).sql
 #Zips the sql backup file and public_html file together and appends the date to the file name.
 zip -r _backup/backup-$(date +%Y-%m-%d)  sql-$(date +%Y-%m-%d).sql public_html
 #Removes the sql file after zipping as there is no need for it lying around.
